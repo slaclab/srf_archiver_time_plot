4c0b9b18374f84da827e6849e5d76a9130f54abc
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-07-11 18:05:39 -0700

    patch

--- plot.py
+++ plot.py
@@ -3,28 +3,25 @@ from pydm import Display
 from lcls_tools.superconducting.sc_linac_utils import ALL_CRYOMODULES
 from lcls_tools.superconducting.scLinac import CRYOMODULE_OBJECTS
 
+
 class Plot(Display):
     def ui_filename(self):
-        """
-        Your code here
-        """
         return "timeplot.ui"
     
     def __init__(self, parent=None, args=None):
         super().__init__(parent=parent, args=args, ui_filename="timeplot.ui")
-        """
-        Your code here
-        """
+
         self.ui.cm_combobox.addItems(ALL_CRYOMODULES)
         self.ui.suffix_line_edit.returnPressed.connect(self.update)
         self.ui.cm_combobox.currentIndexChanged.connect(self.update)
         self.ui.second_spinbox.valueChanged.connect(self.update_time)
         self.ui.plot.setShowLegend(True)
         self.ui.autoscale_checkbox.stateChanged.connect(self.update)
+        self.ui.ymin_spinbox.valueChanged.connect(self.update)
+        self.ui.ymax_spinbox.valueChanged.connect(self.update)
         
     def update_time(self):
         self.ui.plot.setTimeSpan(self.ui.second_spinbox.value())
-    
     
     def update(self):
         self.ui.plot.clearCurves()
@@ -35,7 +32,6 @@ class Plot(Display):
             self.ui.plot.addYChannel(y_channel=cavity.pv_addr(self.ui.suffix_line_edit.text()),
                                      useArchiveData=True)
             
-        if not self.ui.autoscale_checkbox.isChecked():
-            self.ui.plot.setAutoRangeY(False)
-            self.ui.plot.setMinYRange(self.ui.ymin_spinbox.value())
-            self.ui.plot.setMaxYRange(self.ui.ymax_spinbox.value())
+        self.ui.plot.setAutoRangeY(self.ui.autoscale_checkbox.isChecked())
+        self.ui.plot.setMinYRange(self.ui.ymin_spinbox.value())
+        self.ui.plot.setMaxYRange(self.ui.ymax_spinbox.value())
